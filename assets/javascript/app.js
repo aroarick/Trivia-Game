97bef9f5b098fed8ec274ddd164822fd2bb1c6f5
Ashley Roarick <ahorseroar@gmail.com>
2019-09-26 20:54:59 -0500

    finishing texas

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $("#time").hide();
     $("#start").on("click", gamePlay.startGame);
     $(document).on("click", '.choice', gamePlay.checkGuess);
-    // $("#restart").on("click", gamePlay.startGame);
+    
 });
 
 var gamePlay = {
@@ -25,46 +25,46 @@ var gamePlay = {
         choices: ["Dr. Emmet Brown", "George McFly", "Biff Tannen", "Marty McFly"],
         answer: 3
     },
-    // {
-    //     question: "What do the characters in 'Real Genius' fill Jerry's house with",
-    //     choices: ["Post-it Notes", "Popcorn", "Balloons", "Shaving Cream"],
-    //     answer: 1
-    // },
-    // {
-    //     question: "What does Indiana Jones say when confronted with snakes in 'Raider of the Lost Ark?'",
-    //     choices: ["Not snakes!", "Get me outta here!", "Snakes. Why did it have to be snakes.", "Of all the things, it had to be snakes!"],
-    //     answer: 2
-    // },
-    // {
-    //     question: "What is Yoda's famous line in 'The Empire Strikes Back' when he is training with Luke?",
-    //     choices: ["Do or do not. There is no try", "Luminous beings are we, not this crude matter.", "Truly wonderful, the mind of a child is.", "Adventure. Excitement. A Jedi craves not these things."],
-    //     answer: 0
-    // },
-    // {
-    //     question: "What kind of car did Jake Ryan drive in 'Sixteen Candles'?",
-    //     choices: ["Rolls Royce", "BMW", "Ferrari", "Porsche"],
-    //     answer: 3
-    // },
-    // {
-    //     question: "What are the rules for keeping a Gremlin?",
-    //     choices: ["Don't talk to it, don't let it speak, and don't let it eat", "Keep him out of the light, don't give him any water, never feed him after midnight", "Make sure he has light, water him every hour, only feed him at night", "Name him Gizmo, make sure he has friends, keep him happy"],
-    //     answer: 1
-    // },
-    // {
-    //     question: "What iconic line does Bruce Campbell say in 'The Evil Dead' when he replaces his hand with a chainsaw?",
-    //     choices: ["Sweet", "Awesome", "Groovy", "Radical"],
-    //     answer: 2
-    // },
-    // {
-    //     question: "What power does the babe have in 'Labyrinth'?",
-    //     choices: ["the power of voodoo", "the power of hoodoo", "the power of joodoo", "the power of hulu"],
-    //     answer: 0
-    // },
-    // {
-    //     question: "'Die Hard' is a Christmas movie",
-    //     choices: ["False", "True"],
-    //     answer: 1
-    // }
+    {
+        question: "What do the characters in 'Real Genius' fill Jerry's house with",
+        choices: ["Post-it Notes", "Popcorn", "Balloons", "Shaving Cream"],
+        answer: 1
+    },
+    {
+        question: "What does Indiana Jones say when confronted with snakes in 'Raider of the Lost Ark?'",
+        choices: ["Not snakes!", "Get me outta here!", "Snakes. Why did it have to be snakes.", "Of all the things, it had to be snakes!"],
+        answer: 2
+    },
+    {
+        question: "What is Yoda's famous line in 'The Empire Strikes Back' when he is training with Luke?",
+        choices: ["Do or do not. There is no try", "Luminous beings are we, not this crude matter.", "Truly wonderful, the mind of a child is.", "Adventure. Excitement. A Jedi craves not these things."],
+        answer: 0
+    },
+    {
+        question: "What kind of car did Jake Ryan drive in 'Sixteen Candles'?",
+        choices: ["Rolls Royce", "BMW", "Ferrari", "Porsche"],
+        answer: 3
+    },
+    {
+        question: "What are the rules for keeping a Gremlin?",
+        choices: ["Don't talk to it, don't let it speak, and don't let it eat", "Keep him out of the light, don't give him any water, never feed him after midnight", "Make sure he has light, water him every hour, only feed him at night", "Name him Gizmo, make sure he has friends, keep him happy"],
+        answer: 1
+    },
+    {
+        question: "What iconic line does Bruce Campbell say in 'The Evil Dead' when he replaces his hand with a chainsaw?",
+        choices: ["Sweet", "Awesome", "Groovy", "Radical"],
+        answer: 2
+    },
+    {
+        question: "What power does the babe have in 'Labyrinth'?",
+        choices: ["the power of voodoo", "the power of hoodoo", "the power of joodoo", "the power of hulu"],
+        answer: 0
+    },
+    {
+        question: "'Die Hard' is a Christmas movie",
+        choices: ["False", "True"],
+        answer: 1
+    }
 ],
 
     startGame: function () {
@@ -79,6 +79,8 @@ var gamePlay = {
         $('#timer').text(gamePlay.timer);
 
         $('#start').hide();
+
+        $('.typewriter').hide();
 
         $('#time').show();
         $('#game').show();
@@ -146,7 +148,7 @@ var gamePlay = {
 
             gamePlay.correct++;
             clearInterval(gamePlay.timerId);
-            resultId = setTimeout(gamePlay.playerGuess, 1000);
+            resultId = setTimeout(gamePlay.playerGuess, 3000);
             $('#results').html('<h3>Righteous!</h3>');
         }
 
@@ -155,7 +157,7 @@ var gamePlay = {
 
             gamePlay.wrong++;
             clearInterval(gamePlay.timerId);
-            resultId = setTimeout(gamePlay.playerGuess, 1000);
+            resultId = setTimeout(gamePlay.playerGuess, 3000);
             $('#results').html('<h3>Bro, no! It was '+ gamePlay.questions[gamePlay.currentQuestionIndex].choices[gamePlay.questions[gamePlay.currentQuestionIndex].answer] +'</h3>');
         }
     },
